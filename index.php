d1f7b37ffc6cdc2ea2871ba96e02ac9c450e1974
CleliaRezzola <clelia.rezzola@hotmail.com>
2014-08-25 09:49:44 +1200

    removed white spaces

--- index.php
+++ index.php
@@ -9,38 +9,37 @@
 
     <main>
 
-    <?php 
+      <?php 
 
-      if(isset($_GET['pages'])){
-         $page = $_GET['pages'];
-         $pageURI = "includes/".$page.".php";
+        if(isset($_GET['pages'])){
+           $page = $_GET['pages'];
+           $pageURI = "includes/".$page.".php";
 
-         if (file_exists($pageURI)) {
-            require_once ($pageURI);
+           if (file_exists($pageURI)) {
+              require_once ($pageURI);
 
-         }else {
+           }else {
 
-            require_once ('includes/content-404.php');
-         }
-         
-      } else {  
+              require_once ('includes/content-404.php');
+           }
+           
+        } else {  
 
-        require_once ('includes/home.php');
+          require_once ('includes/home.php');
 
-      }   
-    ?>
+        }   
+      ?>
 
-    <?php require_once("includes/content-contact.php") ?>
+      <?php require_once("includes/content-contact.php") ?>
 
     </main>
 
     <?php require_once("includes/footer.php") ?>
-  
+
       <script type="text/javascript" src="assets/js/jquery.js"></script> 
       <script type="text/javascript" src="assets/js/jquery.slabtext.js"></script>  
       <script type="text/javascript" src="assets/js/main.js"></script>
-
-
+      
       <!-- add Modernizr -->
       <script src="assets/js/vendor/modernizr-2.8.0.min.js"></script>
 
@@ -56,10 +55,8 @@
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
         ga('create', 'UA-XXXXX-X', 'auto');
         ga('send', 'pageview');
-
       </script>
     <!-- Bootstrap core JavaScript
     ================================================== -->
