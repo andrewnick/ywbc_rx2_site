b44202fd9b78a9f5d8319410f38e7966dd8c4c2c
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2014-09-17 23:36:57 +1200

    changed postion of / in br tag

--- includes/content-shoeDesc.php
+++ includes/content-shoeDesc.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<h2 class="shoeDesc_text shoeDesc_title col-sm-offset-1 col-sm-11">The RX&sup2 Legionaire</h2>
 			<p class="shoeDesc_text shoeDesc_blurb col-sm-offset-1 col-sm-10 col-md-8 col-lg-7">
-				The Legionaire is our lastest innovation in footwear. </br>
+				The Legionaire is our lastest innovation in footwear. <br/>
 				<strong>And we’re damned proud of it too.</strong></p>
 			<p class="shoeDesc_text comfort_blurb comfort_blurb_expanded col-sm-offset-1 col-sm-6">
 									Passionately engineered as a state of the art,
