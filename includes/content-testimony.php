eb200c5bf9987f5bb7eaebf2f7c4a5063e2aead9
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2014-10-02 21:24:37 +1300

    fixed front page section sizing and slabtext issue

--- includes/content-testimony.php
+++ includes/content-testimony.php
@@ -1,50 +1,49 @@
 <section class="testimony_background hidden-xs">
-	<div class="testimony_container testimony_row container">
+	<div class="testimony_container container">
 		<div class="row">
-			<img src="assets/images/rx2_logo--black.png" alt="Logo" class="col-sm-offset-2 col-sm-2">
-			<div class="col-sm-6">
-				<h2 class="text_heading body_text">
-					<span class="testimony_text_heading-1 slabtext">Made for crossfitters</span>
-					<span class="testimony_text_heading-1 slabtext">by crossfitters</span>
-					<span class="testimony_text_heading-2 slabtext">Because we know</span>
-					<span class="testimony_text_heading-2 slabtext">it's more than a workout</span>
-				</h2>
+			<div class="col-sm-offset-3 col-sm-6">				
+				<div class="row">
+					<img src="assets/images/rx2_logo--black.png" alt="Logo" class="col-sm-3 img-responsive">
+					<div class="col-sm-9">
+						<h2 class="text_heading block_text">
+							<span class="testimony_text_heading-1 slabtext">Made for crossfitters</span>
+							<span class="testimony_text_heading-1 slabtext">by crossfitters</span>
+							<span class="testimony_text_heading-2 slabtext">Because we know</span>
+							<span class="testimony_text_heading-2 slabtext">it's more than a workout</span>
+						</h2>
+					</div>
+				</div>
+				<div class="row testimony_row">
+					<div class="col-sm-6">
+						<a href="#footwear"><img class="testimony_btn img-responsive" src="assets/images/Footwear-Button.png" alt="footware btn"></a>
+					</div>
+					<div class="col-sm-6">
+						<a href="#wodwear"><img class="testimony_btn img-responsive" src="assets/images/Wodwear-Button.png" alt="wodware btn"></a>	
+					</div>
+				</div>
+				<div class="row testimony_row">
+					<div class="col-sm-6">
+						<p class="testimony_text_body">“This gear has transformed my workout. Good looking, high quality
+						 and hassle free.”<br><span class="testimony_text_body_person">- Danielle, Auckland.</span></p>
+					</div>
+					<div class="col-sm-6">
+						<p class="testimony_text_body">“Rx2 is awesome! I can really focus on the workout in the knowledge my 
+						gear can hack it.”<br><span class="testimony_text_body_person">-Christopher, Christchurch.</span></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6">
+						<p class="testimony_text_body">“Truly excellent quality. I recommend it highly.”<br><span class="testimony_text_body_person">-Mike, Waiouru.</span></p>
+					</div>
+					<div class="col-sm-6">
+						<p class="testimony_text_body">“This shit is sweet as!”<br><span class="testimony_text_body_person">-Johnny, Tauranga.</span></p>
+					</div>
+				</div>
+				<div class="row testimony_row">
+					<h5 class="col-sm-12 testimony_text_footer">Find your nearest stockist</h5>
+				</div>
+			</div>		
+
 			</div>
-<!-- 			<div class="block_text col-sm-10">
-				<h2 class="testimony_text_heading-1">Made for crossfitters<h2>
-				<h2 class="testimony_text_heading-1">by crossfitters</h2>
-				<h3 class="testimony_text_heading-2">Because we know<h3>
-				<h3 class="testimony_text_heading-2">it's more than a workout</h3>
-			</div> -->
 		</div>
-		<div class="row">
-			<div class="col-sm-offset-2 col-sm-4">
-				<a href="#footwear"><img class="testimony_btn img-responsive" src="assets/images/Footwear-Button.png" alt="footware btn"></a>
-			</div>
-			<div class="col-sm-4">
-				<a href="#wodwear"><img class="testimony_btn" src="assets/images/Wodwear-Button.png" alt="wodware btn"></a>	
-			</div>
-		</div>
-		<div class="row testimony_row">
-			<div class="col-sm-offset-2 col-sm-4">
-				<p class="testimony_text_body">“This gear has transformed my workout. Good looking, high quality
-				 and hassle free.”<br><span class="testimony_text_body_person">- Danielle, Auckland.</span></p>
-			</div>
-			<div class="col-sm-3">
-				<p class="testimony_text_body">“Rx2 is awesome! I can really focus on the workout in the knowledge my 
-				gear can hack it.”<br><span class="testimony_text_body_person">-Christopher, Christchurch.</span></p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-offset-2 col-sm-4">
-				<p class="testimony_text_body">“Truly excellent quality. I recommend it highly.”<br><span class="testimony_text_body_person">-Mike, Waiouru.</span></p>
-			</div>
-			<div class="col-sm-3">
-				<p class="testimony_text_body">“This shit is sweet as!”<br><span class="testimony_text_body_person">-Johnny, Tauranga.</span></p>
-			</div>
-		</div>
-		<div class="row testimony_row">
-			<h5 class="col-sm-12 testimony_text_footer">Find your nearest stockist</h5>
-		</div>
-	</div>		
 </section>
